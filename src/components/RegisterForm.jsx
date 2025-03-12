0c603145860530f95207d6f5ca0b9764e6c0fb13
TheHeisenBurglar <vidgames16@gmail.com>
2025-03-12 15:29:57 -0400

    login page

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -10,16 +10,18 @@ export default function RegisterForm({handleFunction, darkMode}) {
     background = 'bg-white'
   }
   return (
-    <div className={`grid w-1/4 border-slate-900 border-2 mx-auto p-5 rounded-lg ${background} pt-10`}>
+    <div className={`grid w-1/4 min-w-[400px] border-slate-900 border-2 mx-auto p-5 rounded-lg ${background} pt-10`}>
       <h1 className={darkMode ? "text-2xl text-center text-white" : "text-2xl text-center"}>Register here!</h1>
       <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your name...</h2>
       <Input type='text' placeholder='name' className={darkMode ? "bg-slate-400" : "bg-white"}/>
       <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your type...</h2>
       <Input type='text' placeholder='type' className={darkMode ? "bg-slate-400" : "bg-white"}/>
       <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your email...</h2>
-      <Input type='text' placeholder='email' className={darkMode ? "bg-slate-400" : "bg-white"}/>
+      <Input type='email' placeholder='email' className={darkMode ? "bg-slate-400" : "bg-white"}/>
       <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your phone...</h2>
-      <Input type='text' placeholder='phone' className={darkMode ? "bg-slate-400" : "bg-white"}/>
+      <Input type='phone' placeholder='phone' className={darkMode ? "bg-slate-400" : "bg-white"}/>
+      <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your password...</h2>
+      <Input type='password' placeholder='password' className={darkMode ? "bg-slate-400" : "bg-white"}/>
       <Button text="Register" onClick={handleFunction} className={'mt-5'}/>
     </div>
   );
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Button from '../../components/ui/Button';
+import { useAuthContext } from '../../components/context/AuthContext';
+import Input from '../../components/ui/Input';
 
-export default function Login() {
+export default function Login({handleFunction, darkMode}) {
+  const {login}= useAuthContext();
+  let background = ''
+  if (darkMode) {
+    background = 'bg-slate-700'
+  } else {
+    background = 'bg-white'
+  }
   return (
     <>
       <Helmet>
         <title>Login | Britannicus BMS</title>
       </Helmet>
-      <div>Login</div>
+      <div className={`grid w-1/4 min-w-[400px] border-slate-900 border-2 mx-auto p-5 rounded-lg ${background} pt-10`}>
+        <h1 className={darkMode ? "text-2xl text-center text-white" : "text-2xl text-center"}>Login here!</h1>
+        <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your email...</h2>
+        <Input type='email' placeholder='email' className={darkMode ? "bg-slate-400" : "bg-white"}/>
+        <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Enter your password...</h2>
+        <Input type='password' placeholder='password' className={darkMode ? "bg-slate-400" : "bg-white"}/>
+        <Button text="Login" onClick={handleFunction} className={'mt-5'}/>
+        <div className='w-full border-b-2 border-slate-900 my-2' />
+        <h2 className={darkMode ? "text-xl text-white" : "text-xl"}>Other Options</h2>
+        <Button icon={'FaGoogle'}text={'Sign in using Google'} onClick={login} />
+      </div>
+      
     </>
   );
 }
