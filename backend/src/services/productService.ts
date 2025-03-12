cf99e978346c05bd0476b40b794800075c2b2baf
John France <john.france1@dcmail.ca>
2025-03-12 19:10:05 -0400

    New work on param route

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -2,8 +2,23 @@ import { Product } from "../models/dto/gen-product.dto";
 import { BookDto, SpecCategory } from "../models/dto/book.dto";
 import { MapDto } from "../models/dto/map.dto";
 import { PeriodicalDto } from "../models/dto/periodical.dto";
+import { getQuery } from "../utils/query-builder";
 
+export interface ProductFilters {
+    search?: string,
+    condition?: string,
+    saleOption?: string,
+    stock?: string,
+    category: string,
+}
 
+export const getProducts = async (types: any, filters: any): Promise<Product[]> => {
+    
+    const query = getQuery(types, filters);
+    // return db.runQuery(query); or perhaps further mapping
+     
+    throw new Error("Not Finished")
+}
 
 export const getAllProducts = async (): Promise<Product[]> => {
     throw new Error("Not implemented");
