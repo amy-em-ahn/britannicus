3d08e01291a4757210d2d30945858396b71ecbae
John France <123506803+johndfrance@users.noreply.github.com>
2025-03-12 14:17:21 -0400

    Delete backend/brit-reading-room directory

--- backend/brit-reading-room/src/models/dto/gen-product.dto.ts
+++
@@ -1,5 +0,0 @@
-import { BookDto } from "./book.dto";
-import { MapDto } from "./map.dto";
-import { PeriodicalDto } from "./periodical.dto";
-
-export type Product = MapDto | BookDto | PeriodicalDto
--- backend/brit-reading-room/src/models/dto/sale.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Sale {
-    saleId: number,
-    bookId: number,
-    customerId: number,
-    price: number, 
-    saleDate: Date,
-}
